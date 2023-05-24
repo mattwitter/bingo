6763291b751137cc233b841145272ba20b311d24
Matthew Witter <matthew.witter@sidearmsports.com>
2023-05-24 14:27:20 -0400

    scores

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -233,8 +233,8 @@ export default {
     tier2: false,
     tier3: false,
     tier4: false,
-    team1Score: 35,
-    team2Score: 31,
+    team1Score: 37,
+    team2Score: 33,
     team3Score: 32,
     team4Score: 40,
 
@@ -492,13 +492,13 @@ export default {
    this.team1 = {
       team_name: "Nieve's Naughty Boys",
       members: ["B1G J0HN", "Miilky", "Ya_Boy_Pappy", "He was NR1", "PapaPink"],
-      completedTiles: [this.mystery = false, this.lockpick = true, this.thread = false, this.pureEss = this, this.bracelet = true, this.fireCape = true, this.gnomeScarf = true, this.dragonMed = true, this.seerCull = false, this.zenyte = false, this.venator = true, this.dpick = false, this.vorkathHead = false, this.saraSwordLight = false, this.zulrah = true, this.odiumMalediction = false, this.wardRing = false, this.crystalSeed = false, this.coxPrayer = false, this.holyElixir = false, this.zammy = false, this.bandosArmaArmour = true, this.gwdHilt = false, this.barrows = true, this.acb = true, this.bloodShard = true, this.masori = false, this.voidwaker = false, this.nightmare = false, this.nex = false, this.corpSigil = false, this.coxPurple = false, this.tobPurple = false, this.revWeapon = false ]
+      completedTiles: [this.mystery = false, this.lockpick = true, this.thread = false, this.pureEss = this, this.bracelet = true, this.fireCape = true, this.gnomeScarf = true, this.dragonMed = true, this.seerCull = false, this.zenyte = false, this.venator = true, this.dpick = false, this.vorkathHead = true, this.saraSwordLight = false, this.zulrah = true, this.odiumMalediction = false, this.wardRing = false, this.crystalSeed = false, this.coxPrayer = false, this.holyElixir = false, this.zammy = false, this.bandosArmaArmour = true, this.gwdHilt = false, this.barrows = true, this.acb = true, this.bloodShard = true, this.masori = false, this.voidwaker = false, this.nightmare = false, this.nex = false, this.corpSigil = false, this.coxPurple = false, this.tobPurple = false, this.revWeapon = false ]
     },
     
     this.team2 = {
       team_name: "DASG Whorehouse",
       members: [":Dacoyda:", "Thicc Six", "Renting a gf", "HighAlchemist", "l glizzy l"],
-      completedTiles: [this.mystery = false, this.lockpick = false, this.thread = false, this.pureEss = true, this.bracelet = true, this.fireCape = true, this.gnomeScarf = true, this.dragonMed = false, this.seerCull = true, this.zenyte = true, this.venator = false, this.dpick = true, this.vorkathHead = false, this.saraSwordLight = false, this.zulrah = false, this.odiumMalediction = true, this.wardRing = false, this.crystalSeed = false, this.coxPrayer = false, this.holyElixir = false, this.zammy = false, this.bandosArmaArmour = true, this.gwdHilt = false, this.barrows = true, this.acb = false, this.bloodShard = true, this.masori = false, this.voidwaker = false, this.nightmare = false, this.nex = false, this.corpSigil = false, this.coxPurple = false, this.tobPurple = false, this.revWeapon = false ]
+      completedTiles: [this.mystery = false, this.lockpick = false, this.thread = false, this.pureEss = true, this.bracelet = true, this.fireCape = true, this.gnomeScarf = true, this.dragonMed = false, this.seerCull = true, this.zenyte = true, this.venator = false, this.dpick = true, this.vorkathHead = true, this.saraSwordLight = false, this.zulrah = false, this.odiumMalediction = true, this.wardRing = false, this.crystalSeed = false, this.coxPrayer = false, this.holyElixir = false, this.zammy = false, this.bandosArmaArmour = true, this.gwdHilt = false, this.barrows = true, this.acb = false, this.bloodShard = true, this.masori = false, this.voidwaker = false, this.nightmare = false, this.nex = false, this.corpSigil = false, this.coxPurple = false, this.tobPurple = false, this.revWeapon = false ]
     },
 
     this.team3 = {
